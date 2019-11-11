89c5f7c1ec4e992ccd45f65614db46a7708f989d
TonyTheGreatUA <fpscinema1@gmail.com>
2019-11-11 15:19:28 +0200

    added settings

--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -1,14 +1,32 @@
+/*eslint-disable*/
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import styles from './SettingsScreen.style';
 
-export class SettingsScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
+const user = {
+  first: 'Anton',
+  last: 'Suhorada',
+  phone: '+380992453354',
+  username: '@antonsuhorada',
+  image:
+    'https://scontent.fiev24-1.fna.fbcdn.net/v/t1.0-1/c0.1.320.320a/p320x320/18485987_1303866376395728_3031517757695979358_n.jpg?_nc_cat=107&_nc_oc=AQn-W43cLzhDZTbsUkB92vgyapuNDbvcexCPj3P5Ntc4INYLuRcpW1-FMMt3ZBFOzKE&_nc_ht=scontent.fiev24-1.fna&oh=a7169249b4229571dca708e9d2f1aef4&oe=5E43A73F',
+};
+
+const SettingsScreen = () => {
+  return (
+    <>
+      <View style={styles.profileCard}>
+        <View style={styles.photoView}>
+          <Image style={styles.userPhoto} source={{ uri: user.image }} />
+        </View>
+        <View style={styles.nameView}>
+          <Text style={styles.nameText}>{`${user.first} ${user.last}`}</Text>
+          <Text style={styles.descrInfo}>{`${user.phone}`}</Text>
+          <Text style={styles.descrInfo}>{`${user.username}`}</Text>
+        </View>
       </View>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default SettingsScreen;
---
+++ src/screens/SettingsScreen/SettingsScreen.style.js
@@ -0,0 +1,32 @@
+/*eslint-disable*/
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  profileCard: { flex: 1, flexDirection: 'row' },
+  nameText: {
+    fontSize: 28,
+    fontFamily: 'arial',
+    color: '#8c8886',
+  },
+  userPhoto: {
+    borderColor: '#D3D3D3',
+    borderWidth: 1,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  photoView: {
+    width: '25%',
+    paddingLeft: '5%',
+    paddingTop: '3%',
+  },
+  nameView: {
+    paddingTop: '8%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '75%',
+  },
+  descrInfo: {
+    color: '#6982c2',
+  },
+});
